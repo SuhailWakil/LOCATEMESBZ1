f15d8e5a183252c20250276111471b6b0189abe4
SuhailWakil <suhailwakil@gmail.com>
2023-09-23 17:10:55 -0700

    updates

--- server.js
+++ server.js
@@ -1,34 +1,34 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
-
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-// Serve your static files (e.g., HTML, CSS, JS) here if needed
 app.use(express.static(__dirname + '/'));
 
 io.on('connection', (socket) => {
-    console.log('A user connected');
+    console.log('A socket connected');
 
     socket.on('coordinates', (data) => {
         console.log('Received coordinates:', data);
-        socket.emit('coordinates', data);
-        // Here, you can process and use the received coordinates as needed.
-        // For this example, we're just displaying them in the console.
+
+        // Emit the coordinates data to all connected clients
+        io.emit('coordinates', data);
     });
 
     socket.on('disconnect', () => {
-        console.log('A user disconnected');
+        console.log('A bus disconnected');
     });
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3002;
 server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
 
-app.get('/', (req, res) => {
-    res.send('Hello, World!!!');
+// Create a route to render the HTML page with the received data
+app.get('/index', (req, res) => {
+    // You can provide additional data here if needed
+    res.sendFile(__dirname + '/index.html');
 });
