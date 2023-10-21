5fb07dc1e38d09db93628d2d5fe88a7c71463a3f
SuhailWakil <suhailwakil@gmail.com>
2023-10-21 12:17:45 -0700

    updates

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ io.on('connection', (socket) => {
     });
 });
 
-const port = process.env.PORT || 3002;
+const port = process.env.PORT || 3003;
 server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
